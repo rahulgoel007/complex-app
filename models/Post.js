9d8515854ea04b19090599386b8d3adadf56daf3
Rahul <ra.goel1995@gmail.com>
2020-02-16 12:22:06 +0530

    Is the Current Visitor the Owner of the Post?

--- models/Post.js
+++ models/Post.js
@@ -45,7 +45,7 @@ Post.prototype.create = function() {
 
 }
 
-Post.reusablePostQuery = function(uniqueOperations) {
+Post.reusablePostQuery = function(uniqueOperations, visitorId) {
     return new Promise(async function(resolve, reject) {
         let aggOperations = uniqueOperations.concat([
             {$lookup: {from: "users", localField: "author", foreignField: "_id", as: "authorDocument"}},
@@ -53,6 +53,7 @@ Post.reusablePostQuery = function(uniqueOperations) {
                 title: 1,
                 body: 1,
                 createdDate: 1,
+                authorId: "$author",
                 author: {$arrayElemAt: ["$authorDocument", 0]}
             }}
         ])
@@ -61,6 +62,8 @@ Post.reusablePostQuery = function(uniqueOperations) {
 
         // clean up Author property in each post object
         posts = posts.map(function(post) {
+            post.isVisitorOwner = post.authorId.equals(visitorId)
+            
             post.author = {
                 username: post.author.username,
                 avatar: new User(post.author, true).avatar
@@ -73,7 +76,7 @@ Post.reusablePostQuery = function(uniqueOperations) {
     })
 }
 
-Post.findSingleById = function(id) {
+Post.findSingleById = function(id, visitorId) {
     return new Promise(async function(resolve, reject) {
         if (typeof(id) != "string" || !ObjectID.isValid(id)) {
             reject()
@@ -82,7 +85,7 @@ Post.findSingleById = function(id) {
         
         let posts = await Post.reusablePostQuery([
             {$match: {_id: new ObjectID(id)}}
-        ])
+        ], visitorId)
 
         if (posts.length) {
             console.log(posts[0])
